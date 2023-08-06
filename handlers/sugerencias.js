97fc3bb1d933a58acbf1465b32f854826c59ccea
ThunLigh Dev <99038862+ThunLighDev@users.noreply.github.com>
2023-08-06 13:27:56 -0500

    Create sugerencias.js

---
+++ handlers/sugerencias.js
@@ -0,0 +1,27 @@
+const Discord = require("discord.js");
+const setupSchema = require("../models/sugerencias.js");
+
+module.exports = (client) => {
+    client.on("messageCreate", async (message) => {
+        if (message.author.bot) return;
+
+        const data = await setupSchema.findOne({ guildID: message.guild.id })
+
+        if (!data.guildID || !data.channelID || !message.guild.id || !message.channel.id) return
+
+        const embed = new Discord.EmbedBuilder()
+            .setTitle(`Sugerencia de: ${message.author.username}`)
+            .setDescription(`> ${message.content}`)
+            .setColor("Random")
+            .setThumbnail(message.author.displayAvatarURL({ dynamic: true }))
+            .setTimestamp()
+
+        let Channel = client.channels.cache.get(data.channelID)
+
+        const mensaje = await Channel.send({ embeds: [embed] })
+
+        mensaje.react("👍")
+        mensaje.react("👎")
+
+    })
+}
