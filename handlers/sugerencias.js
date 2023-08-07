a98dd411e02c1a90f8756fded7eea1ee53d838f7
ThunLigh Dev <99038862+ThunLighDev@users.noreply.github.com>
2023-08-07 12:25:17 -0500

    Update sugerencias.js

--- handlers/sugerencias.js
+++ handlers/sugerencias.js
@@ -8,20 +8,20 @@ module.exports = (client) => {
         const data = await setupSchema.findOne({ guildID: message.guild.id })
 
         if (!data.guildID || !data.channelID || !message.guild.id || !message.channel.id) return
+        if (message.channel.id === data.channelID) {
+            const embed = new Discord.EmbedBuilder()
+                .setTitle(`Sugerencia de: ${message.author.username}`)
+                .setDescription(`> ${message.content}`)
+                .setColor("Random")
+                .setThumbnail(message.author.displayAvatarURL({ dynamic: true }))
+                .setTimestamp()
 
-        const embed = new Discord.EmbedBuilder()
-            .setTitle(`Sugerencia de: ${message.author.username}`)
-            .setDescription(`> ${message.content}`)
-            .setColor("Random")
-            .setThumbnail(message.author.displayAvatarURL({ dynamic: true }))
-            .setTimestamp()
+            let Channel = client.channels.cache.get(data.channelID)
 
-        let Channel = client.channels.cache.get(data.channelID)
+            const mensaje = await Channel.send({ embeds: [embed] })
 
-        const mensaje = await Channel.send({ embeds: [embed] })
-
-        mensaje.react("👍")
-        mensaje.react("👎")
-
+            mensaje.react("👍")
+            mensaje.react("👎")
+        }
     })
 }
